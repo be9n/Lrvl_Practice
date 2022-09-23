9004caa27a5c56744b109788c7abecd2c05c3054
be9n <bed.cool48@gmail.com>
2022-09-23 14:56:21 +0300

    new

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Validation\Concerns\FilterEmailValidation;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function username()
+    {
+        $value = request() -> input('identify');
+
+        $field ='';
+        $field = filter_var($value, FILTER_VALIDATE_EMAIL) ? 'email' : 'phone';
+       
+        request()->merge([$field => $value]);
+
+        return $field;
     }
 }
